00916f321623dafa1bdd3713022710eb446cd77c
brimo80 <omer@makeen.io>
2023-04-22 01:19:38 +0300

    test

--- cypress/e2e/addGeofence.cy.js
+++ cypress/e2e/addGeofence.cy.js
@@ -30,8 +30,8 @@ describe("Add Geofence", () => {
 		cy.wait(2000);
 		cy.get('[placeholder="Enter UserDomain"]').type(Cypress.env("USER_DOMAIN"));
 		cy.wait(2000);
-		// cy.get('[placeholder="Enter UserPoolClientId"]').type(Cypress.env("USER_POOL_CLIENT_ID"));
-		// cy.wait(2000);
+		cy.get('[placeholder="Enter UserPoolClientId"]').type(Cypress.env("USER_POOL_CLIENT_ID"));
+		cy.wait(2000);
 		// cy.get('[placeholder="Enter UserPoolId"]').type(Cypress.env("USER_POOL_ID"));
 		// cy.wait(2000);
 		// cy.get('[placeholder="Enter WebSocketUrl"]').type(Cypress.env("WEB_SOCKET_URL"));
