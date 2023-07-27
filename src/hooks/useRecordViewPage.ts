4ba1d4dfd0833424e34655ad622c86cc55e0c9e2
NihaalAftab-Makeen <86765805+NihaalAftab-Makeen@users.noreply.github.com>
2023-07-28 01:03:00 +0500

    add pageViewDuration in LEAVE_PAGE event (#40)

--- src/hooks/useRecordViewPage.ts
+++ src/hooks/useRecordViewPage.ts
@@ -4,13 +4,18 @@ import { EventTypeEnum } from "@demo/types/Enums";
 
 import { record } from "@demo/utils/analyticsUtils";
 
+let pageViewDateTime: Date;
+
 const useRecordViewPage = (pageName: string) => {
 	useEffect(() => {
 		const path = location.pathname;
-
+		pageViewDateTime = new Date();
 		record([{ EventType: EventTypeEnum.VIEW_PAGE, Attributes: { path, pageName } }]);
 
-		return () => record([{ EventType: EventTypeEnum.LEAVE_PAGE, Attributes: { path, pageName } }]);
+		return () => {
+			const pageViewDuration = String((new Date().getTime() - pageViewDateTime.getTime()) / 1000 / 60);
+			record([{ EventType: EventTypeEnum.LEAVE_PAGE, Attributes: { path, pageName, pageViewDuration } }]);
+		};
 	}, [pageName]);
 
 	return {};
