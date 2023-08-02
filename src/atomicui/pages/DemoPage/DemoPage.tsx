f916227e6c4d9c8da91e65c7b452657d141573fe
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2023-08-02 22:46:14 +0500

    removed log

--- src/atomicui/pages/DemoPage/DemoPage.tsx
+++ src/atomicui/pages/DemoPage/DemoPage.tsx
@@ -437,9 +437,6 @@ const DemoPage: React.FC = () => {
 			}
 
 			mapViewRef?.current?.flyTo({ center: lngLat });
-		} else {
-			const mb = mapViewRef.current?.getMaxBounds();
-			console.log({ mb });
 		}
 	};
 
