7e670fc32946841bc8a1bd4eb537b0e1604a0d2c
Dabeer Raza <dabeerofficial@gmail.com>
2023-09-13 21:37:23 +0500

    changed mobile breakpoint to 430px

--- src/hooks/useDeviceMediaQuery.ts
+++ src/hooks/useDeviceMediaQuery.ts
@@ -2,7 +2,7 @@ import useMediaQuery from "./useMediaQuery";
 
 const useDeviceMediaQuery = () => {
 	const isDesktop = useMediaQuery("(min-width: 1024px)");
-	const isMobile = useMediaQuery("(max-width: 425px)");
+	const isMobile = useMediaQuery("(max-width: 430px)");
 	const isMax556 = useMediaQuery("(max-width: 556px)");
 	const isMax766 = useMediaQuery("(max-width: 766px)");
 	const isTablet = !isDesktop && !isMobile;
