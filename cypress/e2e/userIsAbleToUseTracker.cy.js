6f70bbe164754c57f2cb95b4db8d893b425927af
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2023-09-26 09:36:33 +0500

    updated E2E tests

--- cypress/e2e/userIsAbleToUseTracker.cy.js
+++ cypress/e2e/userIsAbleToUseTracker.cy.js
@@ -9,7 +9,7 @@ describe("Tracker", () => {
 		cy.visitDomain(`${Cypress.env("WEB_DOMAIN")}/demo`);
 		cy.get('[data-testid="hamburger-menu"]').click();
 		cy.get('[data-testid="connect-aws-account-button"]').click();
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.get('[placeholder="Enter IdentityPoolId"]').type(`${Cypress.env("IDENTITY_POOL_ID")}`);
 		cy.get('[placeholder="Enter UserDomain"]').type(`${Cypress.env("USER_DOMAIN")}`);
 		cy.get('[placeholder="Enter UserPoolClientId"]').type(`${Cypress.env("USER_POOL_CLIENT_ID")}`);
@@ -19,7 +19,7 @@ describe("Tracker", () => {
 		cy.wait(5000);
 		cy.get("div").should("contain", "Your AWS account is now connected.");
 		cy.get('[data-testid="sign-in-button"]').click();
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.origin(`${Cypress.env("USER_DOMAIN")}`, () => {
 			cy.get(".modal-content.background-customizable.modal-content-mobile.visible-md.visible-lg").then(els => {
 				[...els].forEach(el => {
@@ -38,7 +38,7 @@ describe("Tracker", () => {
 		cy.wait(5000);
 
 		cy.get('[data-testid="hamburger-menu"]').click();
-		cy.wait(2000);
+		cy.wait(5000);
 
 		cy.get("#root").then($root => {
 			const asd = $root.find('[class="amplify-button amplify-field-group__control amplify-button--primary"]');
@@ -50,79 +50,69 @@ describe("Tracker", () => {
 			}
 		});
 
-		cy.wait(7000);
+		cy.wait(5000);
 	});
 
 	it("should allow user to add a tracker and the user should see the notifications for Geofence enter and exit events", () => {
 		cy.get('[class="amplify-flex geofence-button"]').click();
-		cy.wait(4000);
+		cy.wait(5000);
 		cy.get('[placeholder="Enter address or coordinates"]').type("Empire State Building");
-		cy.wait(4000);
+		cy.wait(5000);
 		cy.contains("Empire State Building").click();
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.get('[placeholder="Type unique Geofence Name"]').type(`${geofenceName}`);
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.contains("Save").click();
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.get("div").should("contain", `${geofenceName}`);
 		cy.wait(1000);
 		cy.get('[class="amplify-flex geofence-card-close"]').click();
-		cy.wait(500);
-
+		cy.wait(5000);
 		cy.get('[data-testid="hamburger-menu"]').click();
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.contains("Tracker").click();
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.contains("Continue").click();
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.get('[class="mapboxgl-canvas"]').click("left", { force: true });
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.get('[class="mapboxgl-canvas"]').click("right", { force: true });
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.get('[class="mapboxgl-canvas"]').click("right", { force: true });
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.contains("Save").click();
-		cy.wait(2000);
+		cy.wait(5000);
 		cy.get('[class="amplify-button amplify-field-group__control amplify-button--primary play-pause-button"]').click();
-		cy.wait(2000);
-
+		cy.wait(5000);
 		cy.get(
 			'[class="Toastify__toast Toastify__toast-theme--dark Toastify__toast--info Toastify__toast--close-on-click enter-geofence"]',
 			{ timeout: 50000 }
 		)
 			.should("be.visible")
 			.click({ multiple: true, force: true });
-
 		cy.get(
 			'[class="Toastify__toast Toastify__toast-theme--dark Toastify__toast--info Toastify__toast--close-on-click exit-geofence"]',
 			{ timeout: 50000 }
 		)
 			.should("be.visible")
 			.click({ multiple: true, force: true });
-
-		cy.wait(2000);
-		cy.get('[data-testid="auth-tracker-box-close"]').click();
-
-		cy.get('[class="amplify-flex geofence-button"]').click();
-		cy.wait(2000);
-		cy.contains("Go Back").click();
-		cy.wait(2000);
-		cy.get(`[data-testid="icon-trash-${geofenceName}"]`).click({ force: true });
-		cy.get('[class="amplify-flex geofence-card-close"]').click();
-
-		cy.get('[data-testid="hamburger-menu"]').click();
-		cy.get('[data-testid="sign-out-button"]').click();
-		cy.get('[data-testid="hamburger-menu"]').click();
-
-		cy.get("#root").then($root => {
-			const root = $root.find('[class="amplify-button amplify-field-group__control amplify-button--primary"]');
-			root.length && root[0].innerText === "Sign out"
-				? root[0].click()
-				: cy.get('[data-testid="hamburger-menu"]').click();
-		});
 		cy.wait(5000);
-
+		cy.get('[data-testid="auth-tracker-box-close"]').click();
+		cy.wait(5000);
+		cy.get('[class="amplify-flex geofence-button"]').click();
+		cy.wait(5000);
+		cy.contains("Go Back").click();
+		cy.wait(5000);
+		cy.get(`[data-testid="icon-trash-${geofenceName}"]`).click({ force: true });
+		cy.wait(5000);
+		cy.get('[class="amplify-flex geofence-card-close"]').click();
+		cy.wait(5000);
 		cy.get('[data-testid="hamburger-menu"]').click();
+		cy.wait(5000);
+		cy.get('[data-testid="sign-out-button"]').click();
+		cy.wait(30000);
+		cy.get('[data-testid="hamburger-menu"]').click();
+		cy.wait(5000);
 		cy.get('[data-testid="disconnect-aws-account-button"]').click();
 	});
 });
