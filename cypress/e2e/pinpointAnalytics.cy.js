660fad655123a9de9b7018bb09ef957271e77eec
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2023-10-12 20:18:42 +0500

    [ALS-1642] added pinpoint analytics E2E tests

--- cypress/e2e/pinpointAnalytics.cy.js
+++ cypress/e2e/pinpointAnalytics.cy.js
@@ -1,29 +1,58 @@
 describe("Should record user events correctly", () => {
 	beforeEach(() => {
-		// cy.visitDomain(`${Cypress.env("WEB_DOMAIN")}/demo`);
+		cy.visitDomain(`${Cypress.env("WEB_DOMAIN")}/demo`);
 	});
 
-	it("should record user events correctly", () => {
-		// cy.get('[data-testid="hamburger-menu"]').click();
-		// cy.wait(2000);
-		// cy.contains("Settings").click();
-		// cy.intercept("POST", "**/events").as("postAnalyticEvent");
-		// cy.get('[data-testid="option-item-Map style"]').click();
-		// cy.get('[data-testid="map-style-item-location.aws.com.demo.maps.HERE.Explore"]').click();
-		// cy.wait("@postAnalyticEvent").then(intercepted => {
-		// 	const requestBody = intercepted.request.body;
-		// 	expect(Object.values(Object.values(Object.values(requestBody.BatchItem)[0])[1])[0].EventType).to.equal(
-		// 		"MAP_STYLE_CHANGE"
-		// 	);
-		// });
+	it("should successfully send correct user event to pinpoint", () => {
+		cy.wait(10000);
+		cy.getAllLocalStorage().then(result => {
+			const analyticsEndpointId =
+				result[`${Cypress.env("WEB_DOMAIN")}`]["amazon-location-web-demo_analyticsEndpointId"];
+			cy.get('[data-testid="hamburger-menu"]').click();
+			cy.wait(2000);
+			cy.contains("Settings").click();
+			cy.intercept("POST", "**/events").as("postPinpointEvents");
+			cy.get('[data-testid="option-item-Map style"]').click();
+			cy.wait(2000);
+			cy.get('[data-testid="map-style-item-location.aws.com.demo.maps.HERE.Explore"]').click();
+			cy.wait(2000);
+			cy.wait("@postPinpointEvents").then(intercepted => {
+				const requestBody = intercepted.request.body;
+				const responseBody = intercepted.response.body;
+				expect(Object.values(requestBody["BatchItem"][analyticsEndpointId]["Events"])[0].EventType).to.equal(
+					"MAP_STYLE_CHANGE"
+				);
+				expect(responseBody["Results"][analyticsEndpointId].EndpointItemResponse.Message).to.equal("Accepted");
+				expect(responseBody["Results"][analyticsEndpointId].EndpointItemResponse.StatusCode).to.equal(202);
+				expect(Object.values(responseBody["Results"][analyticsEndpointId].EventsItemResponse)[0].Message).to.equal(
+					"Accepted"
+				);
+				expect(Object.values(responseBody["Results"][analyticsEndpointId].EventsItemResponse)[0].StatusCode).to.equal(
+					202
+				);
+			});
+		});
+	});
 
-		cy.exec(
-			`PINPOINT_IDENTITY_POOL_ID=${Cypress.env("PINPOINT_IDENTITY_POOL_ID")} PINPOINT_APPLICATION_ID=${Cypress.env(
-				"PINPOINT_APPLICATION_ID"
-			)} node extra/fetch-pinpoint-analytics-events/index.js`
-		).then(result => {
-			const data = JSON.parse(result.stdout);
-			cy.log({ data });
+	it.only("should successfully create correct endpoint with the correct event to correct pinpoint application", () => {
+		cy.wait(10000);
+		cy.getAllLocalStorage().then(result => {
+			const analyticsEndpointId =
+				result[`${Cypress.env("WEB_DOMAIN")}`]["amazon-location-web-demo_analyticsEndpointId"];
+			cy.exec("node extra/fetch-pinpoint-analytics-events/index.js", {
+				failOnNonZeroExit: false,
+				env: {
+					PINPOINT_IDENTITY_POOL_ID: `${Cypress.env("PINPOINT_IDENTITY_POOL_ID")}`,
+					PINPOINT_APPLICATION_ID: `${Cypress.env("PINPOINT_APPLICATION_ID")}`,
+					ANALYTICS_ENDPOINT_ID: `${analyticsEndpointId}`
+				}
+			}).then(result => {
+				const response = JSON.parse(result.stdout);
+				expect(response["$metadata"]["httpStatusCode"]).to.equal(200);
+				expect(response["EndpointResponse"]["ApplicationId"]).to.equal(`${Cypress.env("PINPOINT_APPLICATION_ID")}`);
+				expect(response["EndpointResponse"]["Id"]).to.equal(analyticsEndpointId);
+				expect(response["EndpointResponse"]["User"]["UserId"]).to.equal(`AnonymousUser:${analyticsEndpointId}`);
+			});
 		});
 	});
 });
--- extra/fetch-pinpoint-analytics-events/index.js
+++ extra/fetch-pinpoint-analytics-events/index.js
@@ -1,14 +1,24 @@
-import { PinpointClient, GetEventStreamCommand } from "@aws-sdk/client-pinpoint";
+import { GetEndpointCommand, PinpointClient } from "@aws-sdk/client-pinpoint";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 
 const main = async () => {
 	try {
-		console.log(
-			JSON.stringify({
-				PINPOINT_IDENTITY_POOL_ID: process.env.PINPOINT_IDENTITY_POOL_ID,
-				PINPOINT_APPLICATION_ID: process.env.PINPOINT_APPLICATION_ID
-			})
-		);
+		const pinpointIdentityPoolId = process.env.PINPOINT_IDENTITY_POOL_ID;
+		const pinpointApplicationId = process.env.PINPOINT_APPLICATION_ID;
+		const analyticsEndpointId = process.env.ANALYTICS_ENDPOINT_ID;
+		const region = pinpointIdentityPoolId.split(":")[0];
+
+		const credentials = await fromCognitoIdentityPool({
+			identityPoolId: pinpointIdentityPoolId,
+			clientConfig: { region }
+		})();
+		const pinpointClient = new PinpointClient({ credentials, region });
+		const getEndpointCommand = new GetEndpointCommand({
+			ApplicationId: pinpointApplicationId,
+			EndpointId: analyticsEndpointId
+		});
+		const response = await pinpointClient.send(getEndpointCommand);
+		console.log(JSON.stringify(response));
 	} catch (error) {
 		console.error("Error", { error });
 		throw new Error(`Error: ${error}`);
