9666a2cbb49671c137aedb3c5347da29785e8d82
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2023-11-05 19:29:58 +0500

    fix for failing E2E tests

--- cypress/e2e/userIsAbleToUseGeofence.cy.js
+++ cypress/e2e/userIsAbleToUseGeofence.cy.js
@@ -8,27 +8,22 @@ describe("Geofence", () => {
 
 	beforeEach(() => {
 		cy.visitDomain(`${Cypress.env("WEB_DOMAIN")}/demo`);
-		cy.get('[id="Icon"]').click({ force: true });
-		cy.wait(5000);
-		cy.contains("Settings").click();
-		cy.wait(5000);
-		cy.contains("Connect AWS Account").click();
+		cy.get('[data-testid="hamburger-menu"]').click();
+		cy.get('[data-testid="connect-aws-account-button"]').click();
 		cy.wait(5000);
 		cy.get('[placeholder="Enter IdentityPoolId"]').type(`${Cypress.env("IDENTITY_POOL_ID")}`);
 		cy.get('[placeholder="Enter UserDomain"]').type(`${Cypress.env("USER_DOMAIN")}`);
 		cy.get('[placeholder="Enter UserPoolClientId"]').type(`${Cypress.env("USER_POOL_CLIENT_ID")}`);
 		cy.get('[placeholder="Enter UserPoolId"]').type(`${Cypress.env("USER_POOL_ID")}`);
 		cy.get('[placeholder="Enter WebSocketUrl"]').type(`${Cypress.env("WEB_SOCKET_URL")}`);
-		cy.get('[class="amplify-button amplify-field-group__control amplify-button--primary"]').click();
+		cy.get('[data-testid="connect-button"]').click();
 		cy.wait(5000);
-		cy.contains("Connect AWS Account").click();
-		cy.wait(5000);
-		cy.get('[class="amplify-button amplify-field-group__control amplify-button--primary"]').eq(0).click();
+		cy.get("div").should("contain", "Your AWS account is now connected.");
+		cy.get('[data-testid="sign-in-button"]').click();
 		cy.wait(5000);
 		cy.origin(`${Cypress.env("USER_DOMAIN")}`, () => {
 			cy.get(".modal-content.background-customizable.modal-content-mobile.visible-md.visible-lg").then(els => {
 				[...els].forEach(el => {
-					cy.wait(5000);
 					cy.wrap(el)
 						.get('[placeholder="Username"]')
 						.eq(1)
@@ -41,16 +36,26 @@ describe("Geofence", () => {
 				});
 			});
 		});
-		cy.wait(10000);
-		cy.get('[id="Icon"]').click();
 		cy.wait(5000);
-		cy.contains("Sign out").should("exist");
+
+		cy.get('[data-testid="hamburger-menu"]').click();
 		cy.wait(5000);
-		cy.get('[class="amplify-flex geofence-button"]').click();
+
+		cy.get("#root").then($root => {
+			const asd = $root.find('[class="amplify-button amplify-field-group__control amplify-button--primary"]');
+
+			if (asd.length && asd[0].innerText === "Sign in") {
+				asd[0].click();
+			} else {
+				cy.get('[data-testid="hamburger-menu"]').click();
+			}
+		});
+
 		cy.wait(5000);
 	});
 
 	it("should allow user to add, edit and delete geofence", () => {
+		cy.get('[class="amplify-flex geofence-button"]').click();
 		cy.get('[placeholder="Enter address or coordinates"]').type("Rio Tinto Perth Western Australia");
 		cy.wait(4000);
 		cy.contains("Rio Tinto Operations Centre").click();
@@ -68,9 +73,9 @@ describe("Geofence", () => {
 		cy.wait(5000);
 		cy.get(`[data-testid="icon-trash-${geofenceName}"]`).click({ force: true });
 		cy.wait(5000);
-		cy.get('[class="geofences-list-container"]').should("not.contain", `${geofenceName}`);
+		cy.get('[data-testid="geofences-list-container"]').should("not.contain", `${geofenceName}`);
 		cy.wait(5000);
-		cy.get('[class="amplify-flex geofence-card-close"]').click();
+		cy.get('[data-testid="auth-geofence-box-close-button"]').click();
 		cy.wait(5000);
 		cy.get('[data-testid="hamburger-menu"]').click();
 		cy.wait(5000);
