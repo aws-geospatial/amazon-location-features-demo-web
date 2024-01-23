30bd3ab9feb16663e683ffeeeff87789c45ec50a
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-01-23 22:21:25 +0500

    removed package-lock.json, added vite-plugin-inspect

--- vite.library.ts
+++ vite.library.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import eslint from "vite-plugin-eslint";
+import Inspect from "vite-plugin-inspect";
 import svgr from "vite-plugin-svgr";
 
 const packageNames = [
@@ -47,7 +48,11 @@ export default defineConfig(() => {
 				fix: true,
 				failOnError: false
 			}),
-			dts({ insertTypesEntry: true })
+			dts({ insertTypesEntry: true }),
+			Inspect({
+				build: true,
+				outputDir: ".vite-inspect"
+			})
 		],
 		define: {
 			"process.env": process.env,
