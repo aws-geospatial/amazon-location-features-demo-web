b1bf3eacbb73fae4e899212b27ebddc39b247611
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-02-01 00:15:23 +0500

    updated reload conditions

--- src/hooks/useAmplifyAuth.ts
+++ src/hooks/useAmplifyAuth.ts
@@ -41,6 +41,7 @@ const useAmplifyAuth = () => {
 	const { resetStore: resetAmplifyMapStore } = useAmplifyMap();
 	const { t } = useTranslation();
 	const { isDesktop } = useDeviceMediaQuery();
+	console.log({ pathname: window.location.pathname });
 
 	useEffect(() => {
 		const localAppVersion = localStorage.getItem(LOCAL_APP_VERSION) || "";
@@ -48,9 +49,10 @@ const useAmplifyAuth = () => {
 		if (localAppVersion !== APP_VERSION) {
 			localStorage.clear();
 			localStorage.setItem(LOCAL_APP_VERSION, APP_VERSION);
+			window.location.reload();
 		}
 
-		if (!store.identityPoolId) {
+		if (window.location.pathname === DEMO && !store.identityPoolId) {
 			(async () => {
 				await setClosestRegion();
 				const region = localStorage.getItem(FASTEST_REGION) ?? fallbackRegion;
