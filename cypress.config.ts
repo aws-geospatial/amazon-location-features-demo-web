56ce59ef8558aa9aae98c4ec806d69b99c3ce0fc
Hamza Godil <hamzagodil@Hamzas-MacBook-Pro.local>
2024-05-13 17:45:30 +0500

    Cypress Reporting Added

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
 import { cypressBrowserPermissionsPlugin } from "cypress-browser-permissions";
+import { beforeRunHook, afterRunHook } from 'cypress-mochawesome-reporter/lib';
 
 export default defineConfig({
 	viewportWidth: 1024,
 	viewportHeight: 733,
+	retries: 2,
+	video: false,
 	env: {
 		browserPermissions: {
 			notifications: "allow",
@@ -22,12 +25,30 @@ export default defineConfig({
 				}
 			});
 
+			on('before:run', async (details) => {
+				console.log('override before:run');
+				await beforeRunHook(details);
+			});
+		
+			on('after:run', async () => {
+				console.log('override after:run');
+				await afterRunHook();
+			});
+
 			// eslint-disable-next-line no-param-reassign
 			config = cypressBrowserPermissionsPlugin(on, config);
 			return config;
 		},
 		specPattern: "./cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
 		supportFile: "cypress/support/index.js",
+		reporter: "cypress-mochawesome-reporter",
+    	reporterOptions: {
+    		"reportDir": "cypress/reports",
+   		 	"charts": true,
+    		"reportPageTitle": "ALS E2E Report",
+    		"embeddedScreenshots": true,
+    		"inlineAssets": true
+  		},
 		chromeWebSecurity: false,
 		env: {
 			browserPermissions: {
