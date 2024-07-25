7ead7954b50234672fd33387bfed5922ca6e17d3
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-07-26 02:24:10 +0500

    resolved build issue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import appConfig from "./core/constants/appConfig";
 import useAmplifyAuth from "./hooks/useAmplifyAuth";
 import useAmplifyMap from "./hooks/useAmplifyMap";
 import useAws from "./hooks/useAws";
@@ -7,7 +8,7 @@ import useAwsRoute from "./hooks/useAwsRoute";
 import useAwsTracker from "./hooks/useAwsTracker";
 import usePersistedData from "./hooks/usePersistedData";
 import useRecordViewPage from "./hooks/useRecordViewPage";
-import { EventTypeEnum, TriggeredByEnum } from "./types/Enums";
+import { EsriMapEnum, EventTypeEnum, TriggeredByEnum } from "./types/Enums";
 import { record } from "./utils/analyticsUtils";
 import { debounce } from "./utils/debounce";
 import { clearStorage } from "./utils/localstorageUtils";
@@ -15,6 +16,7 @@ import { clearStorage } from "./utils/localstorageUtils";
 export * as theme from "./theme";
 export { default as DemoPage } from "./atomicui/pages/DemoPage/DemoPage";
 export {
+	appConfig as demoAppConfig,
 	useAmplifyAuth,
 	useAmplifyMap,
 	useAws,
@@ -24,6 +26,7 @@ export {
 	useAwsTracker,
 	usePersistedData,
 	useRecordViewPage,
+	EsriMapEnum,
 	EventTypeEnum,
 	TriggeredByEnum,
 	record,
