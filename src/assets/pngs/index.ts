f372845bce870437953064bb49a29665ce1bd61b
wadhawh <130486914+wadhawh@users.noreply.github.com>
2024-11-08 19:23:01 +0530

    updated map style images (#280)
    Co-authored-by: Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>

--- src/assets/pngs/index.ts
+++ src/assets/pngs/index.ts
@@ -1,5 +1,5 @@
-export { default as EsriLightGray } from "./EsriLightGray.png";
-export { default as HereExplore } from "./HereExplore.png";
-export { default as HereHybrid } from "./HereHybrid.png";
-export { default as HereImagery } from "./HereImagery.png";
 export { default as Loading } from "./Loading.png";
+export { default as Standard } from "./Standard.png";
+export { default as Monochrome } from "./Monochrome.png";
+export { default as Hybrid } from "./Hybrid.png";
+export { default as Satellite } from "./Satellite.png";
