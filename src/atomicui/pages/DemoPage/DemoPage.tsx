fb510e8809acb0e151747b6c5a29964a5252f93c
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2025-04-24 21:45:28 +0500

    fixes

--- src/atomicui/pages/DemoPage/DemoPage.tsx
+++ src/atomicui/pages/DemoPage/DemoPage.tsx
@@ -144,7 +144,6 @@ const DemoPage: FC = () => {
 		isUnauthSimulationOpen: show.unauthSimulation,
 		isSettingsOpen: show.settings,
 		isRouteBoxOpen: show.routeBox,
-		closeRouteBox: () => setShow(s => ({ ...s, routeBox: false })),
 		resetAppStateCb: () => setShow(s => ({ ...initShow, stylesCard: s.stylesCard, settings: s.settings }))
 	});
 	const { t } = useTranslation();
