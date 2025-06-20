7f0a32c9a21316f3f988474af16f6dfb5d1042e7
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2025-06-20 20:09:53 +0500

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-
-import "./locales/i18n";
+import "@demo/locales/i18n";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
