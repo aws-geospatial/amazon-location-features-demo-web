c12bf0ac4038d9ab5508f75b72b980c679014245
Zeeshan Sheikh <zeeshan@makeen.io>
2025-08-29 18:00:48 +0500

    fixed aws-sdk issues in vite.config.ts and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
+// vite.config.ts
 import { resolve } from "path";
-
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import eslint from "vite-plugin-eslint";
 import Inspect from "vite-plugin-inspect";
 import svgr from "vite-plugin-svgr";
+
 
 export default defineConfig(() => {
 	return {
@@ -14,17 +15,17 @@ export default defineConfig(() => {
 			svgr(),
 			eslint({
 				fix: true,
-				failOnError: false
+				failOnError: false,
 			}),
 			dts({ insertTypesEntry: true }),
 			Inspect({
 				build: false,
-				outputDir: ".vite-inspect"
+				outputDir: ".vite-inspect",
 			})
 		],
 		define: {
 			global: "window",
-			__vite_process_env_NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+			__vite_process_env_NODE_ENV: JSON.stringify(process.env.NODE_ENV),
 		},
 		resolve: {
 			alias: {
@@ -39,17 +40,20 @@ export default defineConfig(() => {
 				"@demo/utils": resolve(__dirname, "./src/utils"),
 				"@demo/locales": resolve(__dirname, "./src/locales"),
 				axios: "axios/dist/axios.js",
-				"./runtimeConfig": "./runtimeConfig.browser"
-			}
+				"./runtimeConfig": "./runtimeConfig.browser",
+			},
 		},
 		build: {
+			commonjsOptions: {
+				transformMixedEsModules: true,
+			},
 			outDir: "./lib",
 			sourcemap: false,
 			minify: true,
 			lib: {
 				entry: resolve(__dirname, "src/index.ts"),
 				formats: ["es"],
-				fileName: (format, name) => `${name}.${format}.js`
+				fileName: (format, name) => `${name}.${format}.js`,
 			},
 			rollupOptions: {
 				external: [
@@ -65,22 +69,24 @@ export default defineConfig(() => {
 					"react-router-dom",
 					"react-spring-bottom-sheet",
 					"react-toastify",
-					"react-tooltip"
+					"react-tooltip",
 				],
 				output: {
-					assetFileNames: chunkInfo =>
-						chunkInfo.names.includes("amazon-location-features-demo-web.css") ? "style.css" : chunkInfo.names.join("-")
-				}
-			}
+					assetFileNames: (chunkInfo) =>
+						chunkInfo.names.includes("amazon-location-features-demo-web.css")
+							? "style.css"
+							: chunkInfo.names.join("-"),
+				},
+			},
 		},
 		server: {
-			port: 3000
+			port: 3000,
 		},
 		test: {
 			globals: true,
-			environment: 'jsdom',
-			setupFiles: './src/setupTests.ts',
-			css: true, 
-		  },
+			environment: "jsdom",
+			setupFiles: "./src/setupTests.ts",
+			css: true,
+		},
 	};
 });
